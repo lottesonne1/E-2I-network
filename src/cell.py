69393dac79da50fdab91422f75ab83ba1c7344bc
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-08-05 15:56:01 +0200

    update two-comp

--- src/cell.py
+++ src/cell.py
@@ -1,28 +1,50 @@
 from brian2 import *
 from .synapses import get_synapses_eqs
 
-
-def get_membrane_equation(params):
-
-    eqs = """
-    dV/dt = ( %(Gl)f*nS * ( %(El)f*mV -V ) + I ) / ( %(Cm)f * pF ) : volt (unless refractory)
-    I = I0 + gE * ( %(Ee)f*mV -V ) + gI * ( %(Ei)f*mV -V ) : amp
-    I0 : amp
-    gE : siemens
-    gI : siemens
-    """ % params
-
-    return eqs
-
-def get_neuron_group(params):
+def get_neuron_group(params,
+                     model='single-compartement'):
     
-    eqs = get_membrane_equation(params)
+    if model=='single-compartement':
 
-    return NeuronGroup(1, eqs, 
-                    threshold='V > %(Vtresh)f*mV' % params, 
-                    reset='V=%(Vreset)f * mV ' % params, 
-                    refractory=params['Trefrac']*1e-3*second, 
-                    method='euler')
+        eqs = """
+        dV/dt = ( %(Gl)f*nS * ( %(El)f*mV -V ) + I ) / ( %(Cm)f * pF ) : volt (unless refractory)
+        I = I0 + gE * ( %(Ee)f*mV -V ) + gI * ( %(Ei)f*mV -V ) : amp
+        I0 : amp
+        gE : siemens
+        gI : siemens
+        """ % params
+
+        return NeuronGroup(1, eqs, 
+                        threshold='V > %(Vtresh)f*mV' % params, 
+                        reset='V=%(Vreset)f * mV ' % params, 
+                        refractory=params['Trefrac']*1e-3*second, 
+                        method='euler')
+
+    elif model=='two-compartements':
+
+        # soma eq
+        RVs = '%(Ri)f*Mohm * %(El)f*mV + %(RmS)f*Mohm * V + %(RmS)f*Mohm * %(Ri)f*Mohm * I0' % params
+        Rs = '%(Ri)f*Mohm + %(RmS)f*Mohm' % params
+        Ds = ' %(RmS)f*Mohm * %(Ri)f*Mohm * %(CmS)f*pF ' % params
+        eqs = "dVs/dt = ( %s - ( %s ) * Vs ) / ( %s ) : volt (unless refractory)\n" % (RVs, Rs, Ds)
+        # dendrite eq
+        RVd = '%(Ri)f*Mohm * %(El)f*mV + %(RmD)f*Mohm * Vs + %(RmS)f*Mohm * %(Ri)f*Mohm * I' % params
+        Rd = '%(Ri)f*Mohm + %(RmD)f*Mohm' % params
+        Dd = ' %(RmD)f*Mohm * %(Ri)f*Mohm * %(CmD)f*pF ' % params
+        eqs += "dV/dt = ( %s - ( %s ) * V ) / ( %s ) : volt (unless refractory)\n" % (RVd, Rd, Dd)
+        eqs += """
+        I = gE * ( %(Ee)f*mV - V ) + gI * ( %(Ei)f*mV - V ) : amp
+        I0 : amp
+        gE : siemens
+        gI : siemens
+        """ % params
+        print(eqs)
+
+        return NeuronGroup(1, eqs, 
+                           threshold='Vs > %(Vtresh)f*mV' % params, 
+                           reset='Vs=%(Vreset)f * mV ' % params, 
+                           refractory=params['Trefrac']*1e-3*second, 
+                           method='euler')
 
 def single_cell_simulation(params, 
                            exc_events,
@@ -85,23 +107,29 @@ def single_cell_simulation(params,
 if __name__=='__main__':
 
      params = {
-          'Gl':10, # nS, leak conductance ( 1 / membrane resistance)
-          'Cm':200, #pF, membrane capacitance
+          'RmS':150, # M Ohm, membrane resistance Soma
+          'RmD':300, # M Ohm, membrane resistance Dendrite
+          'Ri':100, # M Ohm
+          'CmD':300, #pF, membrane capacitance
+          'CmS':300, #pF, membrane capacitance
           'El': -70, #mV, leak reversal potential / rest potential
           'Ee': 0, #mV, excitatory reversal potential 
           'Ei': -80, #mV, excitatory reversal potential 
           'Vtresh':-50, #mV, spiking threshold
           'Vreset':-70, #mV, post-spike reset level
           'Trefrac':5, #ms, refractory period
+          'dt':1e-4,
                  }
 
      # initialize brian2 "network"
      network = Network(collect())
 
-     cell = get_neuron_group(params)
+     cell = get_neuron_group(params,
+                             model='two-compartements')
      network.add(cell)
-     M = StateMonitor(cell, ['V','I0'], record=0)
+     M = StateMonitor(cell, ['Vs','V'], record=0)
      network.add(M)
+     cell.Vs = params['El']*mV 
      cell.V = params['El']*mV 
      cell.I0 = 0*pA
      network.run(0.1*second)
@@ -111,5 +139,6 @@ if __name__=='__main__':
      network.run(0.1*second)
 
      from .plot import plot_Vm
+     plot_Vm(M.Vs[0]/mV, params)
      plot_Vm(M.V[0]/mV, params)
      show()
